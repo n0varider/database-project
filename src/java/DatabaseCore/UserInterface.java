4379da387443922aa558710ca82e3a16247b02a1
Vishvam Patel <n0varidergm@gmail.com>
2023-10-24 20:45:06 -0500

    Fixed UI & SQL Queries

--- src/java/DatabaseCore/UserInterface.java
+++ src/java/DatabaseCore/UserInterface.java
@@ -1,23 +1,39 @@
 package DatabaseCore;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class UserInterface{
-    Connection c = new DatabaseConnection().connect();
+    private Connection c = new DatabaseConnection().connect();
+    private Statement st;
     private JFrame frame;
+    private JTable table;
     private JTextArea queryInput;
     private JButton confirm;
-    public UserInterface() {
+    public UserInterface() throws SQLException {
         frame = new JFrame("Database UI");
         queryInput = new JTextArea();
         queryInput.setPreferredSize(new Dimension(400, 200));
 
         confirm = new JButton("Confirm Query");
-        confirm.addActionListener(e -> cleanQuery(queryInput.getText()));
+        confirm.addActionListener(e -> {
+            try {
+                String query = queryInput.getText();
+                switch(query.substring(0, query.indexOf(" ")).toLowerCase()) {
+                    case "select":
+                        System.out.println("yes");
+                        drawTable(query);
+                        break;
+                    default:
+                        genericQuery(query);
+                        break;
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
 
         frame.setLayout(new FlowLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -33,19 +49,47 @@ public class UserInterface{
 
     }
 
-    private void cleanQuery(String s) {
-        System.out.println(s + "\n");;
-        System.out.println(
-                s.replaceAll("\\s+", " ")
-                );
+    private String cleanQuery(String s) {
+        return s.replace("\\s+", " ");
     }
 
-    private void executeQuery(String query) {
-        try {
-            Statement s = c.createStatement();
-        } catch (SQLException e) {
-            error();
+    private DefaultTableModel generateTable(ResultSet res) throws SQLException {
+        ResultSetMetaData md = res.getMetaData();
+        DefaultTableModel mod = new DefaultTableModel();
+        int cols = md.getColumnCount();
+        for(int i = 0; i < cols; i++) {
+            mod.addColumn(md.getColumnName(i+1));
         }
+        while(res.next()) {
+            Object[] row = new Object[cols];
+            for(int i = 0; i < cols; i++) {
+                row[i] = res.getObject(i+1);
+            }
+            mod.addRow(row);
+        }
+        return mod;
     }
 
+    private ResultSet selectQuery(String query) throws SQLException {
+        st = c.createStatement();
+        return st.executeQuery(cleanQuery(query));
+    }
+    private int genericQuery(String query) throws SQLException {
+        st = c.createStatement();
+        return st.executeUpdate(query);
+    }
+    
+    private void drawTable(String query) throws SQLException {
+        DefaultTableModel model = generateTable(selectQuery(queryInput.getText()));
+        if(table == null) {
+            table = new JTable(model);
+            frame.add(new JScrollPane(table));
+        }else {
+            table.setModel(model);
+        }
+        table.revalidate();
+        table.repaint();
+        frame.revalidate();
+        frame.repaint();
+    }
 }
