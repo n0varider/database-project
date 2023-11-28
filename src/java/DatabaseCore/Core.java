ce2fc372748be33cd98c15fe887f552237744fe2
Vishvam Patel <n0varidergm@gmail.com>
2023-11-27 20:46:08 -0600

    Added data importing

--- src/java/DatabaseCore/Core.java
+++ src/java/DatabaseCore/Core.java
@@ -1,14 +1,13 @@
-package DatabaseCore;
+package DatabaseCore.Interaction;
 
-import javax.swing.*;
-import java.awt.*;
 import java.io.*;
-import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 public class Core {
 
     static BufferedReader reader;
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, SQLException {
         UserInterface i = new UserInterface();
 
         FilenameFilter filter = (dir, name) -> name.endsWith(".csv");
@@ -17,15 +16,26 @@ public class Core {
                         System.getProperty("user.dir") +
                         System.getProperty("file.separator") +
                         "data";
+
         File dir = new File(filePath);
-        File[] data = dir.listFiles(filter); // Grab all csv files
-        if(data != null) {
-            for(File n : data) {
+        File[] csvfiles = dir.listFiles(filter); // Grab all csv files
+        
+        // TODO create tables
+        // i.genericQuery("CREATE TABLE name (data);");
+        
+        if(csvfiles != null) {
+            for(File n : csvfiles) {
+                
                 reader = new BufferedReader(new FileReader(n));
-                // TODO Process data contents
+                String fileName = n.getName(); // Name of the table
+                List<String> info = reader.lines().toList(); // Data entries in table
+                
+                // Insert data into table. 
+                for(int k = 1; k < info.size(); k++) {
+                    i.genericQuery(String.format("INSERT INTO %s VALUES (%s)", fileName, info.get(k)));
+                }
+                
             }
         }
-
-
     }
 }
