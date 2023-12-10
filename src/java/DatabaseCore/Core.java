da3a4645d1fca80ba919a025d807f9ba17c9fc7f
Vishvam Patel <n0varidergm@gmail.com>
2023-12-09 19:03:40 -0600

    Seller Button

--- src/java/DatabaseCore/Core.java
+++ src/java/DatabaseCore/Core.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class Core {
 
     public static void main(String[] args) throws FileNotFoundException, SQLException {
-        //UserInterface i = new UserInterface();
+
         UI i = new UI();
         DatabaseConnection.getInstance().connect();
         TableCreation tc = TableCreation.getInstance();
--- src/java/DatabaseCore/UI.java
+++ src/java/DatabaseCore/UI.java
@@ -7,6 +7,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 public class UI {
 
@@ -60,8 +64,10 @@ public class UI {
 
     public void buyerButton() {
         text = new JTextArea();
+        JTextArea inf = new JTextArea();
+        inf.setEditable(false);
         JButton confirm = new JButton("View Account");
-        text.setPreferredSize(new Dimension(400, 200));
+        text.setPreferredSize(new Dimension(30, 24));
         text.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
 
         // Returns buyer information based on their buyer_id
@@ -69,15 +75,23 @@ public class UI {
             try {
                 PreparedStatement s = c.getConnection().prepareStatement("SELECT * FROM Buyer b JOIN Account a ON a.account_id = b.account_id WHERE b.buyer_id = ?");
                 s.setString(1, text.getText());
-                generateTable(s.executeQuery());
+                ResultSet res = s.executeQuery();
+                inf.setText(String.format(
+                        "Buyer ID: %s%nAccount ID: %s%nBalance: %s%nJoin Year: %s%n",
+                        res.getObject(1),
+                        res.getObject(3),
+                        res.getObject(6),
+                        res.getObject(7)
+                ));
             } catch (SQLException ex) {
                 throw new RuntimeException(ex);
             }
 
         });
-        redrawFrame(text, confirm);
+        redrawFrame(text, confirm, inf);
     }
 
+    // Unused
     public static void error(String query) {
         text.setText("ERROR > Something went wrong!");
     }
@@ -86,7 +100,7 @@ public class UI {
         text = new JTextArea();
         JButton viewBonds = new JButton("View My Bonds");
         JButton createNewBond = new JButton("Create Bond");
-        text.setPreferredSize(new Dimension(400, 200));
+        text.setPreferredSize(new Dimension(20, 20));
         text.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
 
         viewBonds.addActionListener(e -> {
@@ -98,10 +112,50 @@ public class UI {
                 throw new RuntimeException(ex);
             }
         });
+
+        createNewBond.addActionListener(e -> { createSellerBond(); });
+        redrawFrame(text, viewBonds, createNewBond);
+    }
+
+    private void createSellerBond() {
+        text = new JTextArea();
+        text.setPreferredSize(new Dimension(450, 25));
+        text.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        text.setText("Enter in the following format: ID, value, interest rate, expiration, pay interval, your id");
+        JButton confirm = new JButton("Confirm");
+
+        confirm.addActionListener(e -> {
+
+            // Parameters for creating a bond
+            String[] tokens = text.getText().replaceAll(" ", "").split(",");
+            String date = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+            try {
+                System.out.println(Arrays.stream(tokens).toList());
+                PreparedStatement s = c.getConnection().prepareStatement("INSERT INTO Bond VALUES (?, ?, ?, ?, ?, ?, ?)");
+                s.setString(1, tokens[0]);
+                s.setString(2, tokens[1]);
+                s.setString(3, tokens[2]);
+                s.setString(4, "'" + date + "'");
+                s.setString(5, "'" + tokens[3] + "'");
+                s.setString(6, tokens[4]);
+                s.setString(7, tokens[5]);
+                s.executeUpdate();
+                text.setText(String.format("Successfully created bond!%nID: %s%nValue: %s%nInterest Rate: %s%nCreation: %s%nExpiration: %s%nPay Interval; %s%nYour ID: %s%n", tokens[0], tokens[1], tokens[2], date, tokens[3], tokens[4], tokens[5]));
+                confirm.setText("Go Back");
+                confirm.addActionListener(x -> { sellerButton(); });
+                frame.revalidate();
+                frame.repaint();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        redrawFrame(text, confirm);
     }
 
     public void empButton() {
-
+        // Not implemented
     }
 
     public void adminButton() throws SQLException{
